5215192fb005ac7dc393d6f475b961e7428181c8
Rupesh Parab <rupeshparab.rp@gmail.com>
2022-02-03 22:45:54 +0530

    feat: Make argo optional (#18)

--- async_nbgrader/handlers.py
+++ async_nbgrader/handlers.py
@@ -9,6 +9,8 @@ from notebook.notebookapp import NotebookApp
 from notebook.utils import url_path_join as ujoin
 from tornado import web
 
+from .scheduler import scheduler
+from .tasks import autograde_assignment
 
 class AsyncAutogradeHandler(AutogradeHandler):
     @web.authenticated
@@ -16,34 +18,46 @@ class AsyncAutogradeHandler(AutogradeHandler):
     @check_notebook_dir
     def post(self, assignment_id: str, student_id: str) -> None:
         """Handler for processing autograding request, queues autograding task in amqp"""
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(os.environ.get("RABBITMQ_HOST", "argo-rabbitmq-service")),
-        )
-        channel = connection.channel()
-        namespace = os.environ.get("NAMESPACE")
-        channel.exchange_declare(
-            exchange=namespace,
-            exchange_type="topic",
-            passive=False,
-            durable=True,
-        )
-        body = json.dumps(
-            {
-                "action": "autograde",
-                "notebook_dir": self.settings["notebook_dir"],
-                "course_id": self.api.course_id,
-                "assignment_id": assignment_id,
-                "student_id": student_id,
-                "NB_UID": os.environ.get("NB_UID"),
-                "NB_GID": os.environ.get("NB_GID"),
-                "JUPYTERHUB_API_TOKEN": os.environ.get("JUPYTERHUB_API_TOKEN"),
-            }
-        )
-        channel.basic_publish(
-            exchange=namespace,
-            routing_key="autograde_events",
-            body=body,
-        )
+        if os.environ.get("ARGO_ENABLED") == "true":
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(os.environ.get("RABBITMQ_HOST", "argo-rabbitmq-service")),
+            )
+            channel = connection.channel()
+            namespace = os.environ.get("NAMESPACE")
+            channel.exchange_declare(
+                exchange=namespace,
+                exchange_type="topic",
+                passive=False,
+                durable=True,
+            )
+            body = json.dumps(
+                {
+                    "action": "autograde",
+                    "notebook_dir": self.settings["notebook_dir"],
+                    "course_id": self.api.course_id,
+                    "assignment_id": assignment_id,
+                    "student_id": student_id,
+                    "NB_UID": os.environ.get("NB_UID"),
+                    "NB_GID": os.environ.get("NB_GID"),
+                    "JUPYTERHUB_API_TOKEN": os.environ.get("JUPYTERHUB_API_TOKEN"),
+                }
+            )
+            channel.basic_publish(
+                exchange=namespace,
+                routing_key="autograde_events",
+                body=body,
+            )
+        else:
+            scheduler.add_job(
+                autograde_assignment,
+                "date",
+                args=[
+                    self.settings["notebook_dir"],
+                    self.api.course_id,
+                    assignment_id,
+                    student_id,
+                ],
+            )
         self.write(
             json.dumps(
                 {
@@ -82,7 +96,9 @@ def load_jupyter_server_extension(nbapp: NotebookApp) -> None:
       nbapp (NotebookApp): The Jupyter Notebook application instance.
     """
     if os.environ.get("NBGRADER_ASYNC_MODE", "true") == "true":
-        nbapp.log.info("Starting background processor for nbgrader serverextension")
         nbapp.web_app.add_handlers(".*$", [rewrite(nbapp, x) for x in handlers])
+        if os.environ.get("ARGO_ENABLED") != "true":
+            nbapp.log.info("Starting background processor for nbgrader serverextension")
+            scheduler.start()
     else:
         nbapp.log.info("Skipping background processor for nbgrader serverextension")
